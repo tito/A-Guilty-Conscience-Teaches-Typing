30163a774bc659e83405b09a952556f9b683b095
Andrew Schmidt <drew.schmidt@me.com>
2013-05-29 23:11:58 -0700

    Working on Rotations

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 from kivy.uix.relativelayout import RelativeLayout
 
+class Letter(Label):
+	def __init__(self, **kwargs):
+		super(Letter, self).__init__(**kwargs)
+
+
 
 class Stream(RelativeLayout):
 
@@ -16,32 +21,37 @@ class Stream(RelativeLayout):
 		self.loadstory(story)
 
 	def loadstory(self, story):
-		letters = []
+		self.letters = []
 		currentx, num = 0, 0
 		for currentletter in story:
 			
 			#First let's make a Label with the current letter, in a list of Label instances.
-			letters.append(Label(text=currentletter, x=self.center_x + currentx, y=self.center_y, font_name=dirname(abspath(__file__))+'/data/edunline.ttf'))
+			self.letters.append(Letter(text=currentletter, x=self.center_x + currentx, y=self.center_y, font_name=dirname(abspath(__file__))+'/data/edunline.ttf'))
 			
 			#Then add it to the parent Stream widget:
-			self.add_widget(letters[num])
+			self.add_widget(self.letters[num])
 
 			#This is a bit of a hack for spaces.
 			if currentletter == " ":
-				currentx += letters[num]._label.get_extents("a")[0] #Spaces don't actually register as anything when in a label, so here I'm using "a" to get a width.
+				currentx += self.letters[num]._label.get_extents("a")[0] #Spaces don't actually register as anything when in a label, so here I'm using "a" to get a width.
 			else:
-				currentx += letters[num]._label.get_extents(currentletter)[0]
+				currentx += self.letters[num]._label.get_extents(currentletter)[0]
 
 			num+=1
+		self.storyLength = num
 
 	def move(self):
-		self.x -= 1
-		print self.x
+		self.x += 1
+		num = 0
+		for x in self.letters:
+			self.letters[num].angle += 1
+			#print self.letters[num].angle
+			num += 1
 
 
 
 class GuiltyGame(Widget):
-	stream = ObjectProperty()
+	#stream = ObjectProperty()
 	
 	def update(self, dt):
 		#self.stream.x += 1
