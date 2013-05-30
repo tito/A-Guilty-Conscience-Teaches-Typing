271c5177a1691de9f441f5bfe2a7d614f7bda0a6
Andrew Schmidt <drew.schmidt@me.com>
2013-05-30 00:39:02 -0700

    Rotations are working!
    Got rotations working, plus I think I understand Kivy a little
    better--specifically, how to get the observer to notice changes (just
    make variables into properties)!

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from os.path import dirname, abspath
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, NumericProperty
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
+from kivy.animation import Animation
 from kivy.uix.relativelayout import RelativeLayout
 
 class Letter(Label):
+	angle = NumericProperty()
+
 	def __init__(self, **kwargs):
 		super(Letter, self).__init__(**kwargs)
 
@@ -22,11 +25,11 @@ class Stream(RelativeLayout):
 
 	def loadstory(self, story):
 		self.letters = []
-		currentx, num = 0, 0
+		currentx, num = -300, 0
 		for currentletter in story:
 			
 			#First let's make a Label with the current letter, in a list of Label instances.
-			self.letters.append(Letter(text=currentletter, x=self.center_x + currentx, y=self.center_y, font_name=dirname(abspath(__file__))+'/data/edunline.ttf'))
+			self.letters.append(Letter(text=currentletter, font_size='40sp', x=self.center_x + currentx, y=self.center_y, font_name=dirname(abspath(__file__))+'/data/edunline.ttf'))
 			
 			#Then add it to the parent Stream widget:
 			self.add_widget(self.letters[num])
@@ -40,23 +43,19 @@ class Stream(RelativeLayout):
 			num+=1
 		self.storyLength = num
 
-	def move(self):
+	def move(self, dt):
 		self.x += 1
 		num = 0
-		for x in self.letters:
-			self.letters[num].angle += 1
-			#print self.letters[num].angle
+		for x in self.children:
+			self.letters[num].angle += 120*dt #dt is current framerate. 
 			num += 1
 
 
 
 class GuiltyGame(Widget):
-	#stream = ObjectProperty()
 	
 	def update(self, dt):
-		#self.stream.x += 1
-		#print self.stream.x
-		self.stream.move()
+		self.stream.move(dt)
 
 
 
